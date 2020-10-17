7163a8fcffb4cb4bbba765e7885a2ac0c08b3168
funny-module-5140 <kevin.michael805@gmail.com>
2020-10-17 22:02:58 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -14,6 +14,7 @@ suite_c: "Alfred Tennyson",
 suite_d: "Charlie Chaplin",
 suite_e: "Crumpet the Elf"
 }
+end
   winner = ""
 passengers.each do |suite, name|
   if suite == :suite_a && name.start_with?("A")
